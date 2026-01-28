470d680d306287a7a810edc99afd513d79a89de7
Gachikoi <k1ss.sakura@outlook.com>
2026-01-29 00:10:27 +0800

    feat(web): 通过以移除元素取代隐藏元素，兼容 ios webview 样式行为

--- packages/web/src/routes/app/header.svelte
+++ packages/web/src/routes/app/header.svelte
@@ -4,18 +4,32 @@
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	import SettingPopover from './setting-popover.svelte';
+
+	let isRemoveLogo = $state(false);
+
+	$effect(() => {
+		const mql = window.matchMedia('(min-width: 40rem)');
+		isRemoveLogo = mql.matches;
+		const onChange = () => {
+			isRemoveLogo = mql.matches;
+		};
+		mql.addEventListener('change', onChange);
+		return () => mql.removeEventListener('change', onChange);
+	});
 </script>
 
 <header class="flex h-18 w-full items-center gap-4 px-6">
-	<section class="hidden grow shrink-0 items-center justify-start sm:flex lg:ml-4">
-		<a class="shrink-0" href={resolve('/')}>
-			<img src={rectangleLogo} alt="Shining!" />
-		</a>
-	</section>
+	{#if isRemoveLogo}
+		<section class="flex shrink-0 grow items-center justify-start lg:ml-4">
+			<a class="shrink-0" href={resolve('/')}>
+				<img src={rectangleLogo} alt="Shining!" />
+			</a>
+		</section>
+	{/if}
 
-	<Input placeholder="搜索 Shining！" class="sm:max-w-100 lg:max-w-120 text-base" />
+	<Input placeholder="搜索 Shining！" class="text-base sm:max-w-100 lg:max-w-120" />
 
-	<section class="flex grow shrink-0 items-center justify-end">
+	<section class="flex shrink-0 grow items-center justify-end">
 		<a
 			title="晒你官网"
 			href="https://www.shiningacg.club"
