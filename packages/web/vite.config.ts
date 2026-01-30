31ad4aa21741c46573d9d352da3831940e33db91
Gachikoi <k1ss.sakura@outlook.com>
2026-01-30 19:38:47 +0800

    feat(web): css 兼容性

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,24 @@
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			targets: {
+				chrome: 99 << 16,
+				edge: 99 << 16,
+				firefox: 97 << 16,
+				ios_saf: 16 << 16,
+				safari: 16 << 16,
+				android: 99 << 16,
+				samsung: 16 << 16
+			}
+		}
+	},
+	build: {
+		cssMinify: 'lightningcss'
+	}
 });
