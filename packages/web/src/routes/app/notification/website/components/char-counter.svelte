1fb74a65a077ff7dcb13df58006278788a9c4d8f
Gachikoi <k1ss.sakura@outlook.com>
2026-02-07 16:56:30 +0800

    refactor(web): 去除无用文件

--- packages/web/src/routes/app/notification/website/components/char-counter.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	let { current = 0, max = 0 }: { current: number; max: number } = $props();
-</script>
-
-<span class="text-sm text-zinc-500 dark:text-zinc-400">
-	{current}/{max}
-</span>
