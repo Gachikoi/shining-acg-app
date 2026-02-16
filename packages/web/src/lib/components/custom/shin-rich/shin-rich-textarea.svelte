43e8c7135429404d8d6e6c285b771e7c8bb9bea8
ChangleCat <cthulhu@changlecat.me>
2026-02-16 17:03:52 +0800

    feat(web): 添加 ShinRichTextarea 组件，暂无功能实现

---
+++ packages/web/src/lib/components/custom/shin-rich/shin-rich-textarea.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		placeholder = '请输入内容',
+		...restProps
+	} = $props();
+
+	let isEmpty = $state(true);
+</script>
+
+<div class={cn('relative w-full', className)} {...restProps}>
+	<div class="absolute top-0 left-0 text-muted-foreground {isEmpty ? 'block' : 'hidden'}">
+		{placeholder}
+	</div>
+	<div contentEditable="true"></div>
+</div>
