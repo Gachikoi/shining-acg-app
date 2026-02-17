32869b333d6bdc00d49aa638be5358340470f9ff
ChangleCat <cthulhu@changlecat.me>
2026-02-17 17:54:36 +0800

    feat(web): 扩展 ConfirmDialog 组件的触控区域，同时优化 AlertDialog 内容样式，采用rem替代px

--- packages/web/src/lib/components/ui/alert-dialog/alert-dialog-content.svelte
+++ packages/web/src/lib/components/ui/alert-dialog/alert-dialog-content.svelte
@@ -20,7 +20,7 @@
 		bind:ref
 		data-slot="alert-dialog-content"
 		class={cn(
-			'fixed top-[50%] left-[50%] z-50 grid w-[266px] translate-x-[-50%] translate-y-[-50%] gap-4 overflow-hidden rounded-lg border bg-background p-0 pt-4 shadow-lg duration-200 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95',
+			'fixed top-[50%] left-[50%] z-50 grid w-66.5 translate-x-[-50%] translate-y-[-50%] gap-4 overflow-hidden rounded-lg border bg-background p-0 pt-4 shadow-lg duration-200 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95',
 			className
 		)}
 		{...restProps}
