6392e7475dd067e2aeed878e8065ba866b6fd05a
Changle_cat <cthulhu@changlecat.me>
2026-02-18 17:42:56 +0800

    fix(web): 更新 ConfirmDialog 组件，支持更多关闭方式，并优化 AlertDialog 按钮样式

--- packages/web/src/lib/components/ui/alert-dialog/alert-dialog-footer.svelte
+++ packages/web/src/lib/components/ui/alert-dialog/alert-dialog-footer.svelte
@@ -14,7 +14,7 @@
 	bind:this={ref}
 	data-slot="alert-dialog-footer"
 	class={cn(
-		'flex flex-row gap-0 border-t border-zinc-300 dark:border-zinc-700 [&>button]:flex-1 [&>button]:rounded-none [&>button]:border-0 [&>button]:font-medium [&>button:not(:first-child)]:border-l [&>button:not(:first-child)]:border-l-zinc-300 dark:[&>button:not(:first-child)]:border-l-zinc-700',
+		'flex flex-row gap-0 border-t border-zinc-300 dark:border-zinc-700 [&>button]:flex-1 [&>button]:rounded-none [&>button]:border-0 [&>button]:text-sm [&>button]:font-medium [&>button:not(:first-child)]:border-l [&>button:not(:first-child)]:border-l-zinc-300 dark:[&>button:not(:first-child)]:border-l-zinc-700',
 		className
 	)}
 	{...restProps}
