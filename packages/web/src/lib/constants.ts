9060752ead635d5851d51b33ad7ebf2759ab5b5a
Gachikoi <k1ss.sakura@outlook.com>
2026-02-23 21:41:49 +0800

    fix(web): 导出不存在的库、reroute 逻辑问题

--- packages/web/src/lib/constants.ts
+++ packages/web/src/lib/constants.ts
@@ -5,5 +5,8 @@ export const IS_TEST = env.PUBLIC_IS_TEST !== 'false';
 export const DOMAIN_CONFIG = {
 	app: IS_TEST ? 'test.app.shiningacg.club' : 'app.shiningacg.club',
 	site: IS_TEST ? 'test.www.shiningacg.club' : 'www.shiningacg.club',
-	root: IS_TEST ? 'test.shiningacg.club' : 'shiningacg.club'
+	root: IS_TEST ? 'test.shiningacg.club' : 'shiningacg.club',
+	appSuffix: 'app.shiningacg.club',
+	siteSuffix: 'www.shiningacg.club',
+	rootSuffix: 'shiningacg.club'
 };
